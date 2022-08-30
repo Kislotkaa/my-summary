c62ed5ace8455bd4823f338d39b5e873f11c2c04
Maksihill <Maksihill@yandex.ru>
2022-08-30 21:37:54 +0500

    dynamic update size screen

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../widgets/base_adaptive_home_content.dart';
 import '../../../widgets/base_drawer_content.dart';
@@ -8,10 +9,15 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: controller.key,
-      drawer: BaseDrawerContent(),
-      body: BaseAdaptiveHomeContent(controller: controller),
+    return ScreenUtilInit(
+      rebuildFactor: RebuildFactors.size,
+      builder: (BuildContext context, Widget? child) {
+        return Scaffold(
+          key: controller.key,
+          drawer: const BaseDrawerContent(),
+          body: BaseAdaptiveHomeContent(controller: controller),
+        );
+      },
     );
   }
 }
